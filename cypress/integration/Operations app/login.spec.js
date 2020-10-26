5f9f846940150c3cf38a4ef291ac120b5e1cb814
Carter Moorse <CarterMoorse@yahoo.co.uk>
2020-10-26 16:01:02 +0000

    Added some docs

--- cypress/integration/Operations app/login.spec.js
+++ cypress/integration/Operations app/login.spec.js
@@ -2,6 +2,13 @@
 /// <reference path="../../support/index.d.ts" />
 
 context('Check application login', () => {
+  /**
+   * Login to application using...
+   * @example cy.login(host, user, password)
+   * Documentation and original template can be found at...
+   * @see https://github.com/shelterbox/Mendix-app-testing-template
+   */
+  // const host = Cypress.env('host')
   const host = 'http://localhost:8081/'
   const user = 'MxAdmin'
   const password = Cypress.env('user')[user]
